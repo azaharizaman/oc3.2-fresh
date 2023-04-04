5a1f5b2bd79469ee4c14d8c7beb3bd3723236eee
Azahari Zaman <azaharizaman@gmail.com>
2023-04-04 12:29:06 +0800

    Update was url

--- config/filesystems.php
+++ config/filesystems.php
@@ -42,7 +42,7 @@ return [
             'secret' => env('WAS_SECRET_ACCESS_KEY'),
             'region' => env('WAS_DEFAULT_REGION'),
             'bucket' => env('WAS_BUCKET'),
-            'endpoint' => 'https://s3.wasabisys.com',
+            'endpoint' => env('WAS_URL'),
             'visibility' => 'public'
         ],
 
@@ -52,7 +52,7 @@ return [
             'secret' => env('WAS_SECRET_ACCESS_KEY'),
             'region' => env('WAS_DEFAULT_REGION'),
             'bucket' => env('WAS_BUCKET'),
-            'endpoint' => 'https://s3.wasabisys.com',
+            'endpoint' => env('WAS_URL'),
             'visibility' => 'public'
             
         ],
@@ -62,7 +62,7 @@ return [
             'secret' => env('WAS_SECRET_ACCESS_KEY'),
             'region' => env('WAS_DEFAULT_REGION'),
             'bucket' => env('WAS_BUCKET'),
-            'endpoint' => 'https://s3.wasabisys.com',
+            'endpoint' => env('WAS_URL'),
             'visibility' => 'public',    
         ],
 
@@ -83,7 +83,7 @@ return [
             'secret' => env('WAS_SECRET_ACCESS_KEY'),
             'region' => env('WAS_DEFAULT_REGION'),
             'bucket' => env('WAS_BUCKET'),
-            'endpoint' => 'https://s3.wasabisys.com'
+            'endpoint' => env('WAS_URL'),
         ],
 
     ],
